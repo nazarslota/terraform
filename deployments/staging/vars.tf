a1d9cd3e10eaca31a9aa00620ecf759c5cb80f4b
nazarslota <nazarslota03@gmail.com>
2023-09-27 16:59:27 +0300

    feat: add providers.tf, vars.tf, and terraform.yaml
    Add new files providers.tf, vars.tf, and terraform.yaml to the staging deployment. These files define the Google Cloud provider, variables for the project, region, and zone, and a GitHub Actions workflow for Terraform CI.

---
+++ deployments/staging/vars.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "The ID of the project in GCP"
+  type        = string
+  default     = "unotes-000"
+}
+
+variable "region" {
+  description = "The region where resources should be created in GCP"
+  type        = string
+  default     = "us-east4"
+}
+
+variable "zone" {
+  description = "The zone where resources should be created in GCP"
+  type        = string
+  default     = "us-east4-a"
+}
