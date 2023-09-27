cec98731139cf458514926cfcdcd5794e961a7a9
nazarslota <nazarslota03@gmail.com>
2023-09-27 21:13:32 +0300

    feat: add GCS backend configuration to backends.tf
    fix: add credentials_file_path variable to vars.tf
    
    fix: update .gitignore to ignore deployments/staging/credentials.json
    
    fix: update terraform.yaml to use credentials.json instead of sa_key.json
    
    fix: remove unnecessary resources from main.tf

--- deployments/staging/backends.tf
+++ deployments/staging/backends.tf
@@ -1,7 +1,14 @@
 # https://developer.hashicorp.com/terraform/language/settings/backends/gcs
 terraform {
   backend "gcs" {
-    bucket = "terraform-state-bucket"
+    bucket = "terraform-state-bucket-staging"
     prefix = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~>4.0"
+    }
   }
 }
--- deployments/staging/main.tf
+++ deployments/staging/main.tf
@@ -3,8 +3,4 @@ provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-
-  credentials = file(var.credentials_file_path)
 }
-
-
